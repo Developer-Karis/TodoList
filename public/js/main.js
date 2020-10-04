26657813b4ebf643714830cbad565ec9b3466ac1
SEVERECLICK-dev <papadopouloskaris@gmail.com>
2020-10-04 12:08:37 +0200

    update button delete finish

--- public/js/main.js
+++ public/js/main.js
@@ -15,10 +15,11 @@ let createButtonTwo;
 let createButtonThree;
 let renamePara;
 let confirmRename;
+let confirmDelete;
 
 // Quand on clique sur le bouton Ajouter
 
-myButtonTask.addEventListener("click", () => {
+let startTodoList = () => {
     createTask = document.createElement("div");
     createTask.setAttribute("class", "item");
     divApp.appendChild(createTask);
@@ -57,6 +58,7 @@ myButtonTask.addEventListener("click", () => {
 
     createButtonTwo.addEventListener("click", (e) => {
         if (e.target.nodeName == "I") {
+            // Ne pas afficher le button Valider et Supprimer
             e.target.parentElement.parentElement.firstElementChild.style.display = "none";
             e.target.parentElement.parentElement.firstElementChild.nextElementSibling.nextElementSibling.style.display = "none";
 
@@ -70,13 +72,20 @@ myButtonTask.addEventListener("click", () => {
             // Bouton de confirmation pour renommer
             confirmRename = document.createElement("button");
             confirmRename.innerText = "Renommer";
-            confirmRename.setAttribute("id", "confirmRename");
             e.target.parentElement.parentElement.parentElement.appendChild(confirmRename);
 
             confirmRename.addEventListener("click", () => {
-                e.target.parentElement.parentElement.parentElement.firstElementChild.nextElementSibling.innerText = renamePara.value;
+                // Mettre le nouveau Texte dans le paragraphe
+                e.target.parentElement.parentElement.parentElement.firstElementChild.nextElementSibling.innerText =
+                    e.target.parentElement.parentElement.parentElement.firstElementChild.value;
+
+                // Afficher le paragraphe
                 e.target.parentElement.parentElement.parentElement.firstElementChild.nextElementSibling.style.display = "block";
+
+                // Input pour Renommer la tâche
                 e.target.parentElement.parentElement.parentElement.firstElementChild.style.display = "none";
+
+                // Button de confirmation pour renommer la tâche
                 e.target.parentElement.parentElement.parentElement.lastElementChild.style.display = "none";
 
                 // Réafficher les buttons Valider et Supprimer
@@ -90,8 +99,22 @@ myButtonTask.addEventListener("click", () => {
     createButtonThree = document.createElement("button");
     spanIcons.appendChild(createButtonThree);
 
-    createButtonThree.addEventListener("click", () => {
-        console.log("Supprimer une tâche");
+    createButtonThree.addEventListener("click", (e) => {
+        if (e.target.nodeName == "I") {
+            // Ne pas afficher les buttons Valider et Editer
+            e.target.parentElement.parentElement.firstElementChild.style.display = "none";
+            e.target.parentElement.parentElement.firstElementChild.nextElementSibling.style.display = "none";
+
+            confirmDelete = document.createElement("button");
+            confirmDelete.innerText = "Supprimer";
+            confirmDelete.style.backgroundColor = "red";
+            e.target.parentElement.parentElement.parentElement.appendChild(confirmDelete);
+
+            confirmDelete.addEventListener("click", () => {
+                e.target.parentElement.parentElement.parentElement.style.display = "none";
+            })
+        }
+        e.target.parentElement.parentElement.firstElementChild.nextElementSibling.nextElementSibling.disabled = true;
     })
 
     // Icon Fini
@@ -123,115 +146,16 @@ myButtonTask.addEventListener("click", () => {
         createButtonThree.style.display = "none";
     }
     myInput.value = "";
-});
+}
 
-// Les touches du clavier
-
+// La touche Enter pour créer une tâche
 myInput.addEventListener("keypress", (e) => {
     if (e.key == "Enter") {
-        createTask = document.createElement("div");
-        createTask.setAttribute("class", "item");
-        divApp.appendChild(createTask);
-
-        // Créer le paragraphe
-        para = document.createElement("p");
-        divApp.prepend(para);
-        createTask.prepend(para);
-        para.innerText = myInput.value;
-
-        // Créer le span
-        spanIcons = document.createElement("span");
-        spanIcons.setAttribute("id", "spanIcons");
-        createTask.appendChild(spanIcons);
-
-        createButtonOne = document.createElement("button");
-        spanIcons.appendChild(createButtonOne);
-
-        createButtonOne.addEventListener("click", (e) => {
-            // Récupérer les 3 buttons Valider, Modifier, Supprimer
-            let arr = Array.from(e.target.parentElement.parentElement.children);
-            if (e.target.nodeName == "I") {
-                e.target.parentElement.parentElement.parentElement.style.backgroundColor = "#40A745";
-                e.target.parentElement.parentElement.parentElement.style.color = "white";
-                arr.forEach(element => {
-                    element.style.backgroundColor = "#40A745";
-                });
-                e.target.parentElement.parentElement.firstElementChild.nextElementSibling.style.display = "none";
-                e.target.parentElement.parentElement.firstElementChild.nextElementSibling.nextElementSibling.style.display = "none";
-                e.target.style.color = "black";
-            }
-        })
-
-        createButtonTwo = document.createElement("button");
-        spanIcons.appendChild(createButtonTwo);
-
-        createButtonTwo.addEventListener("click", (e) => {
-            if (e.target.nodeName == "I") {
-                e.target.parentElement.parentElement.firstElementChild.style.display = "none";
-                e.target.parentElement.parentElement.firstElementChild.nextElementSibling.nextElementSibling.style.display = "none";
-
-                // Créer un input pour renommer la tâche
-                renamePara = document.createElement("input");
-                renamePara.setAttribute("id", "renamePara");
-                e.target.parentElement.parentElement.parentElement.firstElementChild.style.display = "none";
-                e.target.parentElement.parentElement.parentElement.prepend(renamePara);
-                renamePara.placeholder = e.target.parentElement.parentElement.parentElement.firstElementChild.nextElementSibling.innerText;
-
-                // Bouton de confirmation pour renommer
-                confirmRename = document.createElement("button");
-                confirmRename.innerText = "Renommer";
-                confirmRename.setAttribute("id", "confirmRename");
-                e.target.parentElement.parentElement.parentElement.appendChild(confirmRename);
-
-                confirmRename.addEventListener("click", () => {
-                    e.target.parentElement.parentElement.parentElement.firstElementChild.nextElementSibling.innerText = renamePara.value;
-                    e.target.parentElement.parentElement.parentElement.firstElementChild.nextElementSibling.style.display = "block";
-                    e.target.parentElement.parentElement.parentElement.firstElementChild.style.display = "none";
-                    e.target.parentElement.parentElement.parentElement.lastElementChild.style.display = "none";
-
-                    // Réafficher les buttons Valider et Supprimer
-                    e.target.parentElement.parentElement.firstElementChild.style.display = "block";
-                    e.target.parentElement.parentElement.firstElementChild.nextElementSibling.nextElementSibling.style.display = "block";
-                })
-            }
-            e.target.parentElement.parentElement.firstElementChild.nextElementSibling.disabled = true;
-        })
-
-        createButtonThree = document.createElement("button");
-        spanIcons.appendChild(createButtonThree);
-
-        createButtonThree.addEventListener("click", () => {
-            console.log("Supprimer une tâche");
-        })
-
-        // Icon Fini
-        iconFini = document.createElement("i");
-        iconFini.setAttribute("class", "fas fa-check-circle fa-2x");
-        iconFini.style.color = "#0AC0EF";
-        createButtonOne.appendChild(iconFini);
-
-        // Icon Modif
-        iconModif = document.createElement("i");
-        iconModif.setAttribute("class", "fas fa-edit fa-2x");
-        iconModif.style.color = "orange";
-        createButtonTwo.appendChild(iconModif);
-
-        // Icon Delete
-        iconDelete = document.createElement("i");
-        iconDelete.setAttribute("class", "fas fa-trash-alt fa-2x");
-        iconDelete.style.color = "red";
-        createButtonThree.appendChild(iconDelete);
-
-        // Créer chaque boutons pour chaque icon
-        if (myInput.value == "") {
-            createTask.style.display = "none";
-            iconFini.style.display = "none";
-            iconModif.style.display = "none";
-            iconDelete.style.display = "none";
-            createButtonOne.style.display = "none";
-            createButtonTwo.style.display = "none";
-            createButtonThree.style.display = "none";
-        }
-        myInput.value = "";
+        startTodoList();
     }
 })
+
+// Le button Ajouter lorsqu'on clique dessus
+myButtonTask.addEventListener("click", () => {
+    startTodoList();
+});
